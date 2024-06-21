4baba1aeb722b2c16f29001125ffe60adeb9e8ac
FedeG <federicogonzalez761@gmail.com>
2024-06-20 21:03:53 -0300

    Add related objects fetch

--- fixtures_extractor/management/commands/extract_fixture.py
+++ fixtures_extractor/management/commands/extract_fixture.py
@@ -2,11 +2,10 @@
 Based on https://github.com/ascaliaio/django-dumpdata-one
 """
 
-import argparse
 import logging
 from pathlib import Path
 
-import yaml
+from django.apps import apps
 from django.core.management.base import BaseCommand
 
 from fixtures_extractor.extra_logging_formatter import ExtraFormatter
@@ -24,7 +23,82 @@ orm_extractor = ORMExtractor()
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            "-a",
+            "--app",
+            type=str,
+            help="App of the start model to dump",
+        )
+        parser.add_argument(
+            "-m",
+            "--model",
+            type=str,
+            help="Name of the start model to dump",
+        )
+        parser.add_argument(
+            "primary_ids",
+            type=int,
+            nargs="+",
+            help="Primary ids of the start model to dump",
+        )
+        parser.add_argument(
+            "-d",
+            "--output_dir",
+            type=str,
+            default="fixtures",
+            help="Output dir for all the resulted fixtures",
+        )
 
     def handle(self, *args, **options):
-        pass
+        app_name = options.get("app")
+        model_name = options.get("model")
+        full_model_name = f'{app_name}.{model_name}'
+
+        filter_key = 'id'
+
+        primary_ids = options.get("primary_ids")
+        output_dir = Path(options.get("output_dir"))
+
+        for primary_id in primary_ids:
+            primary_output_dir = output_dir.joinpath(
+                f"{model_name.lower()}_{primary_id}"
+            )
+            primary_output_dir.mkdir(parents=True, exist_ok=True)
+            output_file = primary_output_dir.joinpath(f'{full_model_name}.json')
+
+            records = self.process_schema(
+                full_model_name=full_model_name,
+                filter_key=filter_key,
+                filter_value=primary_id,
+                output_dir=primary_output_dir,
+            )
+
+            orm_extractor.dump_records(output_file=output_file, records=records)
+
+    def process_schema(self, full_model_name: str, filter_key: str, filter_value: str, output_dir: Path) -> list:
+        schema_records = []
+
+        logger.info(full_model_name)
+
+        base_model_records = orm_extractor.get_records(
+            app_model=full_model_name, filter_key=filter_key, filter_value=filter_value
+        )
+        jsonfy_records = orm_extractor.build_records(full_model_name, base_model_records)
+        schema_records.extend(jsonfy_records)
+
+        Model = apps.get_model(full_model_name)
+
+        for reverse_relation in Model._meta.related_objects:
+            reverse_app_name = reverse_relation.related_model._meta.app_label
+            reverse_model_name = reverse_relation.related_model._meta.model_name
+
+            for record in base_model_records:
+                dependency_records = self.process_schema(
+                    full_model_name=f'{reverse_app_name}.{reverse_model_name}',
+                    filter_key=reverse_relation.field.name,
+                    filter_value=record['id'],
+                    output_dir=output_dir,
+                )
+                schema_records.extend(dependency_records)
+
+        return schema_records
